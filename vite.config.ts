e35e6f4c3fa80d867c6f7cad6bfd47f697356cc1
LucasHVicente <l.vicente9003@gmail.com>
2023-01-05 13:48:00 -0300

    add: github actions workflow to deploy app to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: "mathlio",
   server: {
     port: 3000,
   },
